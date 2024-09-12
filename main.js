b773c12005c139c73cec5d10c496ce6ce7dcf5c3
florjadedan_nayre_de_jesus <florjadedannayredejesus26@gmail.com>
2024-09-12 22:15:26 +0800

    turn pet-card into js template

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector('#pet-card-template')
+const wrapper = document.createDocumentFragment()
+
+
 async function start() {
   const weatherPromise = await fetch('https://api.weather.gov/gridpoints/MFL/110,50/forecast')
   const weatherData = await weatherPromise.json()
@@ -11,7 +15,15 @@ async function petArea() {
   const petPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petData = await petPromise.json()
 
-  console.log(petData)
+  petData.forEach(pet => {
+    const clone = template.content.cloneNode(true)
+
+    clone = document.querySelector('h3').textContent = pet.createDocumentFragment
+
+    wrapper.appendChild(clone)
+
+  });
+  document.querySelector('.list-pet').appendChild(wrapper)
 }
 
 petArea()
